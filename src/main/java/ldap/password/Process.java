ff4268482452f50f0c35778d3aec6786d59ac73e
chinhdoan712 <chinhdoan712@gmail.com>
2019-05-08 21:35:00 +0700

    process received password

--- src/main/java/ldap/password/Process.java
+++ src/main/java/ldap/password/Process.java
@@ -16,37 +16,11 @@ public class Process {
 		String newPassword = password.getNewPassword();
 		String confirmPassword = password.getConfirmPassword();
 
-//		String dn = getDN(uid);
+		String dn = getDN(uid);
 	}
 
 	private String getDN(String uid) {
-		Hashtable env = new Hashtable();
-		String sp = "com.sun.jndi.ldap.LdapCtxFactory";
-		env.put(Context.INITIAL_CONTEXT_FACTORY, sp);
-		String ldapUrl = "ldap://10.30.152.12:389/dc=viettel, dc=vn";
-		env.put(Context.PROVIDER_URL, ldapUrl);
-		DirContext dctx = new InitialDirContext(env);
-
-		String base = "ou=People";
-
-		SearchControls sc = new SearchControls();
-		String[] attributeFilter = { "cn", "mail" };
-		sc.setReturningAttributes(attributeFilter);
-		sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
-
-		String filter = "(&(sn=W*)(l=Criteria*))";
-
-		NamingEnumeration results = dctx.search(base, filter, sc);
-		while (results.hasMore()) {
-			SearchResult sr = (SearchResult) results.next();
-			Attributes attrs = sr.getAttributes();
-
-			Attribute attr = attrs.get("cn");
-			System.out.print(attr.get() + ": ");
-			attr = attrs.get("mail");
-			System.out.println(attr.get());
-		}
-		dctx.close();
+		return "";
 	}
 
 }
